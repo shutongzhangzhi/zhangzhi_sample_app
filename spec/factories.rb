372f1b3d23878680e0f4364b6d2a2057fba4ed46
zhangzhi <305080980@qq.com>
2013-10-02 19:57:57 +0800

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name "json"
-		email "json@qq.com"
+		sequence(:name) {|n| "json #{n}"}
+		sequence(:email) {|n| "person_#{n}@example.com"}
 		password "foobar"
 		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
