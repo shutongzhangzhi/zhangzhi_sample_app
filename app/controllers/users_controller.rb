b754aa4cb32b6cdfe5b54d007aec8bec7074c69a
zhangzhi <305080980@qq.com>
2013-10-14 21:01:26 +0800

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only: [:index, :edit, :update]
+	before_action :signed_in_user, only: [:index, :edit, :update, :following, :followers]
 	before_action :correct_user, only: [:edit, :update]
 	before_action :admin_user, only: :destroy
 	before_action :non_signed_in_user, only: :new
+
+	def following
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.followed_users.paginate(page: params[:page])
+		render 'show_follow'
+	end
+
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
+	end
 
 	def destroy
 		user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,14 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
-  resources :users
+  resources :users do
+	  member do
+		  get :following, :followers
+	  end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   match '/signin',	to: 'sessions#new',	via: 'get'
   match '/signout',	to: 'sessions#destroy',	via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
